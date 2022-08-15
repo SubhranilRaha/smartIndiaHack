50f6cb328a34d1f0710dcd7563ecbc0e08cf3419
Srinjoy Ray <srinjoyray@yahoo.in>
2022-08-15 18:04:09 +0530

    Implemented school controllers

--- server/controller/schoolController.js
+++ server/controller/schoolController.js
@@ -1,4 +1,6 @@
 const { Classroom } = require("../model/ClassRoom");
+const { Teacher } = require("../model/Teacher");
+const { Student } = require("../model/Student");
 
 const createClassroom = async(req,res) => {
     if(req?.user?.userType != "School"){
@@ -18,17 +20,60 @@ const addTeacher = async(req , res) =>{
     if(req?.user?.userType != "School"){
         return res.status(401).json({message : "Access Denied"})
     }
-    // Complete the remaining
-
+    const {classroomId , teacherId} = req.body;
+    let classroom = await Classroom.findOne({ 
+        school: req.user.userInfo._id,
+        _id : classroomId
+    });
+    let teachers = classroom?.teachers;
+    if(teachers?.includes(teacherId) == false){
+        teachers.push(teacherId);
+    }
+    classroom.teachers = teachers;
+    await Classroom.findByIdAndUpdate({_id:classroomId}, classroom);
+    return res.status(200).json(classroom);
 }
 const addStudent = async(req , res) =>{
     if(req?.user?.userType != "School"){
         return res.status(401).json({message : "Access Denied"})
     }
-    // Complete the remaining
+    const {classroomId , studentId} = req.body;
+    let classroom = await Classroom.findOne({ 
+        school: req.user.userInfo._id,
+        _id : classroomId
+    });
+    let students = classroom?.students;
+    if(students?.includes(studentId) == false){
+        students.push(studentId);
+    }
+    classroom.students = students;
+    await Classroom.findByIdAndUpdate({_id:classroomId}, classroom);
+    return res.status(200).json(classroom);
 
+}
+
+const getClassroom = async(req,res) => {
+    if(req?.user?.userType != "School"){
+        return res.status(401).json({message : "Access Denied"})
+    }
+    const classroom = await Classroom.find({ 
+        school: req.user.userInfo._id
+    });
+    return res.status(200).json(classroom);
+}
+
+const getTeacher = async(req,res) => {
+    if(req?.user?.userType != "School"){
+        return res.status(401).json({message : "Access Denied"})
+    }
+    const teachers = await Teacher.find({ 
+        school: req.user.userInfo._id
+    });
+    return res.status(200).json(teachers);
 }
 
 module.exports.createClassroom = createClassroom;
 module.exports.addTeacher = addTeacher;
 module.exports.addStudent = addStudent;
+module.exports.getTeacher = getTeacher;
+module.exports.getClassroom = getClassroom;
